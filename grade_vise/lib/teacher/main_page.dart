01f5cbe928ae062ff08fccb4c5b51c7027496994
Abhishek <abhisheksangule6@gmail.com>
2025-03-15 01:09:15 +0530

    complete the bottom nev style just made it widget and reuse everywhere

--- grade_vise/lib/teacher/main_page.dart
+++ grade_vise/lib/teacher/main_page.dart
@@ -3,11 +3,11 @@ import 'package:grade_vise/teacher/screens/Grading.dart';
 import 'package:grade_vise/teacher/screens/classroom_details.dart';
 import 'package:grade_vise/teacher/screens/meet.dart';
 import 'package:grade_vise/teacher/screens/uses_list.dart';
-import 'package:grade_vise/utils/colors.dart';
 
 class MainPage extends StatefulWidget {
   final String classroomId;
   final String userPhoto;
+
   const MainPage({
     super.key,
     required this.classroomId,
@@ -19,12 +19,19 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  int index1 = 0;
+  int selectedIndex = 0;
+  final List<IconData> icons = [
+    Icons.home_outlined,
+    Icons.show_chart,
+    Icons.videocam_outlined,
+    Icons.person_outline,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: IndexedStack(
-        index: index1,
+        index: selectedIndex,
         children: [
           ClassroomDetails(
             classroomId: widget.classroomId,
@@ -39,54 +46,69 @@ class _MainPageState extends State<MainPage> {
         height: 85,
         decoration: BoxDecoration(
           color: Colors.white,
-
           borderRadius: BorderRadius.only(
             topRight: Radius.circular(50),
             topLeft: Radius.circular(50),
           ),
           boxShadow: [
             BoxShadow(
-              color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.6),
-              blurRadius: 80,
-              spreadRadius: 0,
+              color: Colors.black.withOpacity(0.2),
+              blurRadius: 10,
+              spreadRadius: 2,
             ),
           ],
         ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              _buildNavItem(0, Icons.home_outlined),
-              _buildNavItem(1, Icons.show_chart),
-              _buildNavItem(2, Icons.videocam_outlined),
-              _buildNavItem(3, Icons.person_outline),
-            ],
-          ),
+        child: Stack(
+          children: [
+            // Animated background effect
+            AnimatedPositioned(
+              duration: Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+              left:
+                  (MediaQuery.of(context).size.width / 4.45) * selectedIndex +
+                  30, // Move based on index
+              top: 12,
+              child: Container(
+                width: 55,
+                height: 55,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 8.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: List.generate(
+                  icons.length,
+                  (index) => _buildNavItem(index, icons[index]),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 
   Widget _buildNavItem(int itemIndex, IconData iconData) {
-    final bool isSelected = index1 == itemIndex;
+    final bool isSelected = selectedIndex == itemIndex;
 
-    return InkWell(
+    return GestureDetector(
       onTap: () {
         setState(() {
-          index1 = itemIndex;
+          selectedIndex = itemIndex;
         });
       },
-      borderRadius: BorderRadius.circular(16),
       child: Container(
-        padding: EdgeInsets.all(12),
-        decoration:
-            isSelected
-                ? BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(16),
-                )
-                : null,
+        width: 55, // Match background width
+        height: 55,
+        alignment: Alignment.center,
         child: Icon(
           iconData,
           size: 28,
