a77147ff808d191e63e2711aa4ecebb44db5a59b
Abhishek <abhisheksangule6@gmail.com>
2025-03-24 23:16:03 +0530

    change in student_meet

--- grade_vise/lib/student/main_page.dart
+++ grade_vise/lib/student/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grade_vise/student/Student_meet.dart';
 import 'package:grade_vise/student/classroom_screen.dart';
 import 'package:grade_vise/student/my_grades.dart';
 import 'package:grade_vise/teacher/screens/meet.dart';
@@ -79,7 +80,7 @@ class _MainPageScreenState extends State<MainPageScreen> {
                     photoUrl: widget.photoUrl,
                   ),
                   MyGrades(),
-                  Meet(),
+                  Student_Meet(),
                   PeoplePage(
                     classroomId: widget.classroomId,
                     name: widget.classData['name'],
