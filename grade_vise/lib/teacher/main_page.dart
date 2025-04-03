67a5cadab90d573815c86ed47d845c16b35a4715
Abhishek <abhisheksangule6@gmail.com>
2025-04-04 00:40:28 +0530

    solve the unknow bug

--- grade_vise/lib/teacher/main_page.dart
+++ grade_vise/lib/teacher/main_page.dart
@@ -128,7 +128,6 @@ class _MainPageState extends State<MainPage> {
                   Grading(
                     classroomId: widget.classroomId,
                     students: widget.students,
-                    assignment: widget.assignments,
                   ),
                   Meet(
                     uid: widget.uid,
